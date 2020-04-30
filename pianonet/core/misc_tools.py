00bf4acaa330f7e03328c3233cad3fdbe59363e4
angsten <tangst10@gmail.com>
2020-04-30 15:31:51 -0500

    Add misc tool function for generating evenly spaced but noisy points

---
+++ pianonet/core/misc_tools.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+def get_noisily_spaced_floats(start, end, num_points):
+    """
+    start: starting float of range
+    end: ending float of range
+    num_points: number of floats to output
+
+    Returns evenly spaced floats from start to end inclusive with random noise added to each point.
+    Example: get_noisily_spaced_floats(start=0.8, end=1.2, num_points=3) could give
+             the result array([0.84758304, 1.04413777, 1.18226883])
+    """
+
+    evenly_spaced_points = np.linspace(start=start, stop=end, num=(num_points + 1), endpoint=True)[:-1]
+
+    noise_to_add_array = np.random.random(num_points) * ((end - start) / num_points)
+
+    return evenly_spaced_points + noise_to_add_array
