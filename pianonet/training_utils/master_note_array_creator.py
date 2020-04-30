dd6fde41109acc9e89505e0010699165d2d676ea
angsten <tangst10@gmail.com>
2020-04-29 19:38:19 -0500

    Complete master note array get_instance method

--- pianonet/training_utils/master_note_array_creator.py
+++ pianonet/training_utils/master_note_array_creator.py
@@ -1,10 +1,9 @@
-from pianonet.core.pianoroll import Pianoroll
-from pianonet.core.note_array import NoteArray
-from pianonet.core.note_array_creator import NoteArrayCreator
+import random
+
+import numpy as np
 
 from pianonet.core.midi_tools import get_midi_file_paths_list
-
-import random
+from pianonet.core.pianoroll import Pianoroll
 
 
 class MasterNoteArrayCreator(object):
@@ -23,10 +22,17 @@ class MasterNoteArrayCreator(object):
                  stretch_range=None,
                  left_padding_timesteps=0,
                  ):
+        """
+        path_to_directory_of_midi_files: String path to directory of midi files one level deep in file tree
+        note_array_creator: NoteArrayCreator instance specifying how to crop and downsample a pianoroll into a NoteArray
+        num_augmentations_per_midi_file: How many stretched pianorolls to create from each midi fil
+        stretch_range: A tupe of two floats in range (0.0, infinity) specifying valid random range for stretch fractions
+        left_padding_timesteps: How much padding in timesteps to add between two concatentated pianorolls
+        """
         self.path_to_directory_of_midi_files = path_to_directory_of_midi_files
         self.note_array_creator = note_array_creator
         self.num_augmentations_per_midi_file = num_augmentations_per_midi_file
-        self.stretch_range = stretch_range
+        self.stretch_range = stretch_range if (stretch_range != None) else [1.0, 1.0]
         self.left_padding_timesteps = left_padding_timesteps
 
     def get_instance(self):
@@ -43,22 +49,47 @@ class MasterNoteArrayCreator(object):
             c. Add NoteArray instances to a list
         3. Concat the full list of NoteArray instances into a single master NoteArray instance
         """
+
         midi_file_paths_list = get_midi_file_paths_list(self.path_to_directory_of_midi_files)
 
         random.shuffle(midi_file_paths_list)
 
-        # [print(m) for m in midi_file_paths_list]
+        [print(m) for m in midi_file_paths_list]
 
         note_arrays_list = []
 
         for midi_file_path in midi_file_paths_list:
 
+            print("Using file at " + midi_file_path)
+
             pianoroll = Pianoroll(midi_file_path)
 
+            pianoroll = pianoroll[0:100] #####remove!!
+
             for i in range(self.num_augmentations_per_midi_file):
+                print("i = " + str(i))
+
                 stretch_fraction = random.uniform(self.stretch_range[0], self.stretch_range[1])
+
+                print("Stretch_fraction is " + str(stretch_fraction))
                 stretched_pianoroll = pianoroll.get_stretched(stretch_fraction=stretch_fraction)
 
                 stretched_pianoroll.add_zero_padding(left_padding_timesteps=self.left_padding_timesteps)
 
                 note_array = self.note_array_creator.get_instance(pianoroll=stretched_pianoroll)
+
+                note_arrays_list.append(note_array)
+
+        print("***" + str(len(note_arrays_list)))
+
+        total_note_array_size = np.sum([note_array.get_length() for note_array in note_arrays_list])
+
+        master_note_array_values = np.zeros((total_note_array_size,)).astype('bool')
+
+        concat_index = 0
+        for note_array in note_arrays_list:
+            master_note_array_values[concat_index:concat_index + note_array.get_length()] = note_array.array
+
+            concat_index += note_array.get_length()
+
+        return self.note_array_creator.get_instance(flat_array=master_note_array_values)
