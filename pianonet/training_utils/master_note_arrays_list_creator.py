519ea2b8efa7c8d007dc727b038b10bf98b76339
angsten <tangst10@gmail.com>
2020-04-30 16:34:45 -0500

    Add get_length_in_timesteps helper funciton to NoteArray class

--- pianonet/training_utils/master_note_arrays_list_creator.py
+++ pianonet/training_utils/master_note_arrays_list_creator.py
@@ -3,6 +3,8 @@ import numpy as np
 from pianonet.core.midi_tools import get_midi_file_paths_list
 from pianonet.core.pianoroll import Pianoroll
 
+from pianonet.core.misc_tools import get_noisily_spaced_floats
+
 
 class MasterNoteArrayCreator(object):
     """
@@ -18,20 +20,23 @@ class MasterNoteArrayCreator(object):
                  note_array_creator,
                  num_augmentations_per_midi_file=1,
                  stretch_range=None,
-                 left_padding_timesteps=0,
+                 start_padding_timesteps=0,
+                 end_padding_timesteps=0,
                  ):
         """
         path_to_directory_of_midi_files: String path to directory of midi files one level deep in file tree
         note_array_creator: NoteArrayCreator instance specifying how to crop and downsample a pianoroll into a NoteArray
         num_augmentations_per_midi_file: How many stretched pianorolls to create from each midi fil
         stretch_range: A tupe of two floats in range (0.0, infinity) specifying valid random range for stretch fractions
-        left_padding_timesteps: How much padding in timesteps to add between two concatentated pianorolls
+        start_padding_timesteps: How much padding in timesteps to add between two concatentated pianorolls
+        end_padding_timesteps: How much padding in timesteps to add between two concatentated pianorolls
         """
         self.path_to_directory_of_midi_files = path_to_directory_of_midi_files
         self.note_array_creator = note_array_creator
         self.num_augmentations_per_midi_file = num_augmentations_per_midi_file
         self.stretch_range = stretch_range if (stretch_range != None) else [1.0, 1.0]
-        self.left_padding_timesteps = left_padding_timesteps
+        self.start_padding_timesteps = start_padding_timesteps
+        self.end_padding_timesteps = end_padding_timesteps
 
     def get_instance(self):
         """
@@ -60,20 +65,11 @@ class MasterNoteArrayCreator(object):
 
             pianoroll = Pianoroll(midi_file_path)
 
-            pianoroll = pianoroll[0:100]  #####remove!!
+            pianoroll = pianoroll[0:400]  #####remove!!
 
-            stretch_step_size = (self.stretch_range[1] - self.stretch_range[0]) / self.num_augmentations_per_midi_file
-            random_noise_magnitude = 0.5 * stretch_step_size
-
-            stretch_fractions = np.linspace(start=self.stretch_range[0] + random_noise_magnitude,
-                                            stop=self.stretch_range[1] - random_noise_magnitude,
-                                            num=self.num_augmentations_per_midi_file,
-                                            endpoint=True
-                                            )
-
-            random_noise_array = random_noise_magnitude * (
-                        np.random.random_sample(self.num_augmentations_per_midi_file) - 0.5)
-            stretch_fractions = (stretch_fractions + random_noise_array)
+            stretch_fractions = get_noisily_spaced_floats(start=self.stretch_range[0],
+                                                          end=self.stretch_range[1],
+                                                          num_points=self.num_augmentations_per_midi_file)
 
             print("Stretch_fractions array:", str(stretch_fractions))
 
@@ -85,7 +81,8 @@ class MasterNoteArrayCreator(object):
                 # print("Stretch_fraction is " + str(stretch_fraction))
                 stretched_pianoroll = pianoroll.get_stretched(stretch_fraction=stretch_fraction)
 
-                stretched_pianoroll.add_zero_padding(left_padding_timesteps=self.left_padding_timesteps)
+                stretched_pianoroll.add_zero_padding(left_padding_timesteps=self.start_padding_timesteps,
+                                                     right_padding_timesteps=self.end_padding_timesteps)
 
                 note_array = self.note_array_creator.get_instance(pianoroll=stretched_pianoroll)
 
