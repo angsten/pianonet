2577a8be851038fe82191e6409ba46c33181d4f4
angsten <tangst10@gmail.com>
2020-05-02 13:17:41 -0500

    Update to draft three of training generator. Add generator funciton.

--- pianonet/training_utils/note_sample_generator.py
+++ pianonet/training_utils/note_sample_generator.py
@@ -6,58 +6,110 @@ class NoteSampleGenerator(object):
     Class representing a generator of NoteArray input and target segments that can be used for training.
     """
 
-    def __init__(self, note_arrays_list, sample_length_in_timesteps, batch_size, random_seed=0):
-        self.note_arrays_list = note_arrays_list
-        self.sample_length_in_timesteps = sample_length_in_timesteps
+    def __init__(self,
+                 master_note_array,
+                 num_notes_in_model_input,
+                 num_predicted_notes_in_sample,
+                 batch_size,
+                 random_seed=0):
+
+        self.master_note_array = master_note_array
+        self.num_notes_in_model_input = num_notes_in_model_input
+        self.num_predicted_notes_in_sample = num_predicted_notes_in_sample
         self.batch_size = batch_size
         self.random_seed = random_seed
 
-        self.num_note_arrays = len(self.note_arrays_list)
+        self.total_notes = self.master_note_array.note_array.get_length_in_notes()
 
-        valid_sample_points_table = np.zeros((100, 2), dtype='int')
+        prediction_start_indices = np.arange(start=0,
+                                             stop=self.total_notes,
+                                             step=self.num_predicted_notes_in_sample)
 
-        valid_sample_points_list = []
+        print("Prediction start indices: ", str(prediction_start_indices))
 
-        last_timestep_index = 0
-        for note_array_index in range(self.num_note_arrays):
-            note_array = self.note_arrays_list[note_array_index]
+        np.random.seed(self.random_seed)
 
-            print("Note array length: " + str(note_array.get_length_in_timesteps()))
-
-            valid_sample_timesteps = np.arange(
-                start=0,
-                stop=note_array.get_length_in_timesteps(),   ####if not modded correctly, could result in too large of index!
-                step=self.sample_length_in_timesteps,
-                dtype='int',
-            )
-
-            for sample_timestep in valid_sample_timesteps:
-                valid_sample_points_list.append((note_array_index, sample_timestep))
-
-
-        print(valid_sample_points_list)
-
-        # np.random.seed(self.random_seed)
+        self.randomized_prediction_start_indices = prediction_start_indices
 
         # NOW SHUFFLE IT!!!!!!!!!!!!!!!!!!!!!!!!
 
-    def __iter__(self):
-        # sample an index, say ind = 5000
-        #
-        # need to find which note array this corresponds to - find first point when larger than cumsum value
-        #
+    def get_input_sample_index_range(self, prediction_start_index):
+        """
+        Get the start and end indices of the sample input using the following scheme:
 
-        counter = 0
-        while (True):
+            num_notes_in_model_input          num_predicted_notes_in_sample - 1
+        | - - - - - - - - - - - - - - | | - - - - - - - - - - - - - - - - - - - - - |
 
-            inputs = []
-            targets = []
+        0 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 0 1 0 0 0 0 0 0 0 1 0 0 0 0 1 0 0 0 0 0 0 1 0
+        ^                               ^                                           ^
+        |                               |                                           |
+        start_index                     prediction_start_index                      end_index
+        """
 
-            for i in range(self.batch_size):
+        start_index = prediction_start_index - self.num_notes_in_model_input
 
-                yield np.array(inputs, targets)
+        end_index = prediction_start_index + (self.num_predicted_notes_in_sample - 1)
 
-                counter += 1
+        return (start_index, end_index)
 
-    def save(self):
-        pass
+    # def __iter__(self):
+    #     return iter(self.batch_generator())
+
+    def get_batch_generator(self):
+
+        def batch_generator():
+            counter = 0
+
+            while (True):
+
+                inputs = []
+                targets = []
+
+                for i in range(self.batch_size):
+                    prediction_start_index = self.randomized_prediction_start_indices[counter]
+
+                    print(prediction_start_index)
+
+                    input_index_range = self.get_input_sample_index_range(
+                        prediction_start_index=prediction_start_index)
+
+                    print("Input index range:" + str(input_index_range))
+
+                    target_index_range = (input_index_range[0] + 1, input_index_range[1] + 1)
+
+                    bounded_input_index_range = (max(input_index_range[0], 0),
+                                                 min(input_index_range[1], self.total_notes - 1))
+
+                    bounded_target_index_range = (max(target_index_range[0], 0),
+                                                  min(target_index_range[1], self.total_notes - 1))
+
+                    input = self.master_note_array.note_array.array[
+                            bounded_input_index_range[0]:bounded_input_index_range[1]]
+                    target = self.master_note_array.note_array.array[
+                             bounded_target_index_range[0]:bounded_target_index_range[1]]
+
+                    if input_index_range[0] < 0:
+                        pad_count = abs(input_index_range[0])
+                        input = np.pad(array=input, pad_width=(pad_count, 0), mode='constant').astype('bool')
+                    elif input_index_range[1] >= self.total_notes:
+                        pad_count = input_index_range[1] - (self.total_notes - 1)
+                        input = np.pad(array=input, pad_width=(0, pad_count), mode='constant').astype('bool')
+
+                    if target_index_range[0] < 0:
+                        pad_count = abs(target_index_range[0])
+                        target = np.pad(array=target, pad_width=(pad_count, 0), mode='constant').astype('bool')
+                    elif target_index_range[1] >= self.total_notes:
+                        pad_count = target_index_range[1] - (self.total_notes - 1)
+                        target = np.pad(array=target, pad_width=(0, pad_count), mode='constant').astype('bool')
+
+                    inputs.append(input)
+                    targets.append(target)
+
+                    counter += 1
+
+                    if counter >= len(self.randomized_prediction_start_indices):
+                        counter = 0
+
+                yield (np.array(inputs), np.array(targets))
+
+        return batch_generator()
