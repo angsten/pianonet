aa46beaf2d9bff7532b329c1c7f1815717f668a3
angsten <tangst10@gmail.com>
2020-05-04 18:55:32 -0500

    Refactor to use NoteArrayTransformer to do transformations and hold transforming attributes

--- pianonet/core/note_array.py
+++ pianonet/core/note_array.py
@@ -1,7 +1,5 @@
 import numpy as np
 
-from pianonet.core.pianoroll import Pianoroll
-
 
 class NoteArray(object):
     """
@@ -17,49 +15,31 @@ class NoteArray(object):
     [0, 0, 0, 1, 0, 0, 0, 0, ... 1, 0, 0, 1, 0, 0, 0, 0, ...]
     """
 
-    def __init__(self, pianoroll=None, flat_array=None, min_key_index=0, num_keys=128, resolution=1.0):
+    def __init__(self, pianoroll=None, flat_array=None, note_array_transformer=None):
         """
         pianoroll: Instance of Pianoroll class used to populate the notearray's array
         flat_array: Optionally can initialize from a 1D array of note states. **This 1D array is assumed to already
                     be cropped and downsampled at the specified parameters given to this constructor**
-        min_key_index: array index of the lowest piano key to not crop
-        num_keys: number of piano keys in pianoroll to keep. min_key_index + num_keys - 1 gives the index of the highest
-                  included key.
-        resolution: Float between 0 and 1 controlling how much to downsample the original pianoroll array.
-                    resolution=0.5 means every other note is sampled. Must be 1.0 if a flat_array is given
+        note_array_transformer: NoteArrayTransformer instance for converting a pianoroll or flat array into a NoteArray
         """
 
-        if (pianoroll != None) and isinstance(flat_array, np.ndarray):
+        self.note_array_transformer = note_array_transformer
+
+        pianoroll_is_defined = (pianoroll != None)
+        flat_array_is_defined = isinstance(flat_array, np.ndarray)
+
+        if pianoroll_is_defined and flat_array_is_defined:
             raise Exception("Cannot use both a pianoroll and flat_array initializer. Choose one.")
 
-        if (min_key_index + num_keys) > 128:
-            raise Exception(
-                "(min_key_index + num_keys) = " + str(min_key_index + num_keys) + ", which is greater than 128")
+        elif pianoroll_is_defined:
+            self.array = self.note_array_transformer.get_flat_array_from_pianoroll(pianoroll=pianoroll)
 
-        if resolution > 1.0:
-            raise Exception("Provided resolution of " + str(resolution) + " is greater than 1.0.")
-
-        self.min_key_index = min_key_index
-        self.num_keys = num_keys
-        self.resolution = resolution
-
-        if isinstance(flat_array, np.ndarray):
-            if (flat_array.shape[0] % num_keys) != 0:
-                raise Exception("flat_array contains a timestep with a partial key state. This is not allowed.")
-
-            self.time_steps = (flat_array.shape[0] // num_keys)
+        elif flat_array_is_defined:
+            self.note_array_transformer.validate_flat_array(flat_array)
             self.array = flat_array.copy()
+
         else:
-            pianoroll = pianoroll.get_copy()
-
-            if self.resolution != 1.0:
-                pianoroll.stretch(stretch_fraction=resolution)
-
-            self.time_steps = pianoroll.array.shape[0]
-
-            cropped_pianoroll = pianoroll.array[:, min_key_index:(min_key_index + num_keys)]
-
-            self.array = cropped_pianoroll.flatten()
+            raise Exception("Neither a pianoroll nor a flat_array initializer has been provided.")
 
     def get_pianoroll(self):
         """
@@ -67,19 +47,7 @@ class NoteArray(object):
         A Pianoroll instance is returned.
         """
 
-        cropped_unflattened_pianoroll_array = np.reshape(self.array, (self.time_steps, self.num_keys))
-
-        unflattened_pianoroll_array = np.zeros((self.time_steps, 128)).astype('bool')
-
-        unflattened_pianoroll_array[:,
-        self.min_key_index:self.min_key_index + self.num_keys] = cropped_unflattened_pianoroll_array
-
-        pianoroll_low_resolution = Pianoroll(unflattened_pianoroll_array)
-
-        if self.resolution == 1.0:
-            return pianoroll_low_resolution
-        else:
-            return pianoroll_low_resolution.get_stretched(stretch_fraction=(1.0 / self.resolution))
+        return self.note_array_transformer.get_pianoroll_from_flat_array(flat_array=self.array)
 
     def get_length_in_notes(self):
         """
@@ -93,7 +61,7 @@ class NoteArray(object):
         Returns as an integer the length of the note array in timesteps
         """
 
-        return (self.get_length_in_notes() // self.num_keys)
+        return (self.get_length_in_notes() // self.note_array_transformer.num_keys)
 
     def get_values_in_range(self, start_index, end_index, use_zero_padding_for_out_of_bounds=False):
         """
@@ -102,6 +70,7 @@ class NoteArray(object):
         use_zero_padding_for_out_of_bounds: If true, zeros are returned for those indices in the range that are
                                             out of bounds.
         """
+
         pad_count_at_start = 0
         pad_count_at_end = 0
 
@@ -109,7 +78,6 @@ class NoteArray(object):
         bounded_end_index = min(end_index, self.get_length_in_notes())
 
         values = self.array[bounded_start_index:bounded_end_index]
-
 
         if start_index < 0:
             pad_count_at_start = abs(start_index)
