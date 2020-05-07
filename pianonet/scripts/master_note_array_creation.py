5803049370f89e877319bcdc09cc2e26bd22b89d
angsten <tangst10@gmail.com>
2020-05-07 16:26:18 -0500

    Finalize note array creation script

--- pianonet/scripts/master_note_array_creation.py
+++ pianonet/scripts/master_note_array_creation.py
@@ -8,8 +8,10 @@
 #              where idx is a counter updated to the next unique integer to avoid overwriting.
 ###
 
-import sys
 import json
+import os
+import sys
+import random
 
 from pianonet.core.midi_tools import get_midi_file_paths_list
 from pianonet.core.note_array_transformer import NoteArrayTransformer
@@ -20,48 +22,99 @@ def main():
     arguments = sys.argv
 
     if len(arguments) != 3:
-        print("Rerun with the proper arguments. Usage:")
-        print("python master_note_array_creation.py /path/to/input/file.json /path/to/output/master_note_array.mna")
-
+        print("Rerun with the proper arguments. Example usage:\n")
+        print(" $ python master_note_array_creation.py /path/to/input/file.json /path/to/output/master_note_array.mna")
+        print()
         return
 
     input_json_file_path = sys.argv[1]
-    save_path = sys.argv[2]
+    save_directory_path = sys.argv[2]
 
-    custom_parameters = json.load(input_json_file_path)
+    with open(input_json_file_path, 'rb') as json_file:
+        custom_parameters = json.load(json_file)
 
-    print(custom_parameters)
+    print("\nGenerating master note array using the following parameters:")
+    for k, v in custom_parameters.items():
+        print("\t" + str(k) + ": " + str(v))
 
-    return
+    file_name_prefix = custom_parameters['file_name_prefix']
 
-    path_to_directory_of_midi_files = '../data/midi/bach/'
+    min_key_index = custom_parameters['min_key_index']
+    num_keys = custom_parameters['num_keys']
+    resolution = custom_parameters['resolution']
 
-    min_key_index = 34
-    num_keys = 64
-    resolution = 1.0
+    end_padding_time_steps = custom_parameters['end_padding_time_steps']
+    stretch_range = custom_parameters['stretch_range']
+    num_augmentations_per_midi_file = custom_parameters['num_augmentations_per_midi_file']
 
-    end_padding_time_steps = 96
-    stretch_range = (0.85, 1.2)
-    num_augmentations_per_midi_file = 1
-    time_steps_crop_range = (0, 4000)
+    if custom_parameters['time_steps_crop_range'] != []:
+        time_steps_crop_range = custom_parameters['time_steps_crop_range']
+    else:
+        time_steps_crop_range = None
 
-    note_array_transformer = NoteArrayTransformer(min_key_index=min_key_index, num_keys=num_keys, resolution=resolution)
+    if custom_parameters['stretch_range'] != []:
+        stretch_range = custom_parameters['stretch_range']
+    else:
+        stretch_range = None
+
+    path_to_directory_of_midi_files = custom_parameters['midi_locator']['path_to_midi_file_directory']
+    whitelisted_midi_file_names = custom_parameters['midi_locator']['whitelisted_midi_file_names']
+
+    validation_fraction = custom_parameters['validation_fraction']
+    training_fraction = 1.0 - validation_fraction
 
     midi_file_paths_list = get_midi_file_paths_list(path_to_directory_of_midi_files)
 
-    print("Generatign note array using the following file paths:")
-    [print(path) for path in midi_file_paths_list]
+    if len(whitelisted_midi_file_names) != 0:
+        midi_file_paths_list = [file_path for file_path in midi_file_paths_list if
+                                (os.path.basename(file_path) in whitelisted_midi_file_names)]
 
-    master_note_array = MasterNoteArray(
-        midi_file_paths_list=midi_file_paths_list,
-        note_array_transformer=note_array_transformer,
-        num_augmentations_per_midi_file=num_augmentations_per_midi_file,
-        stretch_range=stretch_range,
-        end_padding_time_steps=end_padding_time_steps,
-        time_steps_crop_range=time_steps_crop_range,
-    )
+    total_midi_files_count = len(midi_file_paths_list)
+    training_midi_files_count = int(training_fraction * total_midi_files_count)
+    validation_midi_files_count = total_midi_files_count - training_midi_files_count
 
-    master_note_array.save(file_path=save_path)
+    random.shuffle(midi_file_paths_list)
+
+    midi_file_paths_split = {
+        'training': midi_file_paths_list[0:training_midi_files_count],
+        'validation': midi_file_paths_list[
+                      training_midi_files_count:training_midi_files_count + validation_midi_files_count]
+    }
+
+    for set_name in ['training', 'validation']:
+        if (set_name == 'validation') and (validation_midi_files_count == 0):
+            print("No files included in the validation master note array. Skipping its creation.")
+            continue
+
+        set_midi_file_paths_list = midi_file_paths_split[set_name]
+
+        print("\nGenerating {set_name} master note array using the these midi file paths:".format(set_name=set_name))
+        [print("\t" + path) for path in set_midi_file_paths_list]
+
+        note_array_transformer = NoteArrayTransformer(min_key_index=min_key_index, num_keys=num_keys,
+                                                      resolution=resolution)
+
+        master_note_array = MasterNoteArray(
+            midi_file_paths_list=set_midi_file_paths_list,
+            note_array_transformer=note_array_transformer,
+            num_augmentations_per_midi_file=num_augmentations_per_midi_file,
+            stretch_range=stretch_range,
+            end_padding_time_steps=end_padding_time_steps,
+            time_steps_crop_range=time_steps_crop_range,
+        )
+
+        i = 0
+        while (True):
+            save_path = os.path.join(save_directory_path, file_name_prefix + "_" + set_name + "_" + str(i) + ".mna")
+
+            if not os.path.exists(save_path):
+                break
+
+            i += 1
+
+        print("Saving note array to file at " + save_path)
+
+        master_note_array.save(file_path=save_path)
 
 
 if __name__ == '__main__':
