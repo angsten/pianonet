b2d7081f39cae7b6cc06cbdba3f51ba98d290c72
angsten <tangst10@gmail.com>
2020-05-12 20:41:05 -0500

    Add helper function to get conv1d model's input shape

---
+++ pianonet/model_building/get_model_input_shape.py
@@ -0,0 +1,20 @@
+
+
+
+def get_model_input_shape(model):
+    """
+    Returns the required (minimum) input size of a FCNN model by analyzing the conv1d layers.
+
+    model: Fully convolutional neural net keras model for which the input shape is determined.
+    """
+
+    model_input_size = 1
+
+    for layer in model.layers:
+        if layer.name.find('conv') != -1:
+            dilation_rate = layer.dilation_rate[0]
+            kernel_size = layer.kernel_size[0]
+
+            model_input_size += (kernel_size - 1)*dilation_rate
+
+    return model_input_size
