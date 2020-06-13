7a68346cc3acaf5635e40ac17edb380721f5b723
angsten <tangst10@gmail.com>
2020-06-13 18:37:49 -0400

    Fix bug in input shape counting when num blocks is more than one

--- pianonet/model_building/get_model_input_shape.py
+++ pianonet/model_building/get_model_input_shape.py
@@ -5,13 +5,12 @@ def get_model_input_shape(model):
     model: Fully convolutional neural net keras model for which the input shape is determined.
     """
 
-    model_input_size = 1
-
+    model_input_size = 0
     for layer in model.layers:
         if layer.name.find('conv') != -1:
             dilation_rate = layer.dilation_rate[0]
             kernel_size = layer.kernel_size[0]
 
-            model_input_size += (kernel_size - 1) * dilation_rate
+            model_input_size += (kernel_size - 1) * max(dilation_rate, 2)
 
     return model_input_size
