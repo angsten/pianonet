a23051e97d1af3ea40b880dfe7cb68db9e4afcf7
angsten <tangst10@gmail.com>
2020-06-16 14:01:59 -0500

    Add filter increments and custom activation function in model parameters

--- pianonet/scripts/model_generators/basic_convolutional_with_blocks.py
+++ pianonet/scripts/model_generators/basic_convolutional_with_blocks.py
@@ -8,6 +8,11 @@
 #              The output model will be saved to
 #
 #                   /path/to/model/output/directory
+# Model parameters should include either a 'filters' key with a list or list of lists describing the absolute numbers
+# of filters in each block's layer, or a filter_increments list or list of lists describing the additional filters
+# relative to the last layer.
+# 'default_activation' should be used to specify the default activation function to use, such as 'relu' or 'elu', when
+# a filter entry does not specifically specify one.
 ###
 
 import pickle
@@ -16,7 +21,7 @@ import sys
 import numpy as np
 
 np.random.seed(0)  # should ensure consistent model weight initializations
-from tensorflow.keras.layers import Input, Conv1D, Activation, LayerNormalization
+from tensorflow.keras.layers import Input, Conv1D, Activation
 from tensorflow.keras.models import Model
 
 
@@ -37,10 +42,31 @@ def main():
     with open(parameters_file_path, 'rb') as params_file:
         model_params = pickle.load(params_file)
 
-    filters = model_params['filters']
+    if 'filters' in model_params:
+        filters = model_params['filters']
 
-    if not isinstance(filters[0], list):
-        filters = [filters]
+        if not isinstance(filters[0], list):
+            filters = [filters]
+    else:
+        filters = []
+        filter_increments = model_params['filter_increments']
+
+        if not isinstance(filter_increments[0], list):
+            filter_increments = [filter_increments]
+
+        filter_count = 0
+        for filter_increments_list in filter_increments:
+            filters_list = []
+            for filter_increment in filter_increments_list:
+                filter_count += filter_increment
+                filters_list.append(filter_count)
+
+            filters.append(filters_list)
+
+    default_activation = model_params.get('default_activation', 'relu')
+
+    def default_dilation_function(layer_index):
+        return 2 ** layer_index
 
     ######################
     ### MODEL BUILDING ###
@@ -53,9 +79,12 @@ def main():
         block_filters = filters[block_id]
 
         for i in range(0, len(block_filters)):
-            conv = Conv1D(filters=block_filters[i], kernel_size=2, strides=1, dilation_rate=2 ** i, padding='valid')(
-                conv)
-            conv = Activation('relu')(conv)
+            conv = Conv1D(filters=block_filters[i],
+                          kernel_size=2,
+                          strides=1,
+                          dilation_rate=default_dilation_function(i),
+                          padding='valid')(conv)
+            conv = Activation(default_activation)(conv)
 
     outputs = Conv1D(filters=1, kernel_size=1, strides=1, dilation_rate=1, padding='valid')(conv)
     outputs = Activation('sigmoid')(outputs)
